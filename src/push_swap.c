3498b07835c92c62027837c64c98505598d24efd
Cristina Fernández <84771182+cristina-fb@users.noreply.github.com>
2021-09-07 20:57:34 +0200

    3 & 5 sorting

--- src/push_swap.c
+++ src/push_swap.c
@@ -46,13 +46,15 @@ int	is_valid_arg(int argc, char **argv)
 	return (1);
 }
 
-void	get_args(int argc, char **argv, t_list **lst)
+int	get_args(int argc, char **argv, t_list **lst)
 {
 	int		i;
 	int		j;
-	char	**split;
+	int		n;
+	char		**split;
 
 	i = 1;
+	n = 0;
 	while (i <= argc)
 	{
 		j = 0;
@@ -63,10 +65,12 @@ void	get_args(int argc, char **argv, t_list **lst)
 			{
 				add_to_list(split[j], lst);
 				free(split[j++]);
+				n++;
 			}
 			free(split);
 		}
 	}
+	return (n);
 }
 
 void	repeated_elements(t_list **lst)
@@ -93,16 +97,246 @@ void	repeated_elements(t_list **lst)
 	}
 }
 
+int	is_sorted(t_list **lst)
+{
+	t_list	*aux;
+	
+	aux = *lst;
+	if (aux)
+	{
+		while (aux->next)
+		{
+			if ((*(int *)aux->content) > (*(int *)aux->next->content))
+				return (0);
+			aux = aux->next;
+		}
+	}
+	return (1);
+}
+
+void	sort_3n(t_list **lst, int n_elem)
+{
+	if (!is_sorted(lst))
+	{
+		if ((*(int *)lst[0]->content) > (*(int *)lst[0]->next->content))
+			write(1, "ra\n", 3);
+			rotate(lst, n_elem);
+		else
+			write(1, "rra\n", 4);
+			r_rotate(lst, n_elem);
+		if (!is_sorted(lst))
+		{
+			write(1, "sa\n", 3);
+			swap(lst, n_elem);
+			if (!is_sorted(lst))
+			{
+				write(1, "ra\n", 3);
+				rotate(lst, n_elem);
+			}
+		}
+	}
+}
+
+void	sort_5n(t_list **lst_a, int n_elem_a)
+{
+	t_list	*lst_b;
+	int	n_elem_b;
+	int	min;
+	int	max;
+	int	pos;
+	
+	lst_b = NULL;
+	n_elem_b = 0;
+	min = 0;
+	max = 0;
+	if (!is_sorted(lst_a))
+	{
+		while (n_elem_a > 3)
+		{
+			max = find_min_max(lst_a, 0);
+			min = find_min_max(lst_a, 1);
+			write(1, "pb\n", 3);
+			push(lst_a, &lst_b, &n_elem_a, &n_elem_b);
+		}
+		sort_3n(lst_a, n_elem_a);
+		while (n_elem_b > 0)
+		{
+			if ((*(int *)lst_a[0]->content) < min)
+			{
+				pos = find_position(lst_a, min);
+				if (pos <= n_elem_a / 2)
+				{
+					while (pos-- > 0)
+					{
+						write(1, "ra\n", 3);
+						rotate(lst_a, n_elem_a);
+					}
+					write(1, "pa\n", 3);
+					push(lst_b, &lst_a, &n_elem_b, &n_elem_a);
+				}	
+				else
+				{
+					while (pos-- > 0)
+					{
+						write(1, "rra\n", 3);
+						r_rotate(lst_a, n_elem_a);
+					}
+					write(1, "pa\n", 3);
+					push(lst_b, &lst_a, &n_elem_b, &n_elem_a);
+				}
+				min = *(int *)lst_a[0]->content;
+			}
+			else if ((*(int *)lst_a[0]->content) > max)
+			{
+				pos = find_position(lst_a, max);
+				if (pos >= n_elem_a / 2)
+				{
+					while (pos-- > 0)
+					{
+						write(1, "ra\n", 3);
+						rotate(lst_a, n_elem_a);
+					}
+					write(1, "pa\n", 3);
+					push(lst_b, &lst_a, &n_elem_b, &n_elem_a);
+				}	
+				else
+				{
+					while (pos-- > 0)
+					{
+						write(1, "rra\n", 3);
+						r_rotate(lst_a, n_elem_a);
+					}
+					write(1, "pa\n", 3);
+					push(lst_b, &lst_a, &n_elem_b, &n_elem_a);
+				}
+				max = *(int *)lst_a[0]->content;
+			}
+			else
+			{
+				pos = find_position_insert(lst_a, *(int *)lst_b[0]->content);
+				if (pos <= n_elem_a / 2)
+				{
+					while (pos-- > 0)
+					{
+						write(1, "ra\n", 3);
+						rotate(lst_a, n_elem_a);
+					}
+					write(1, "pa\n", 3);
+					push(lst_b, &lst_a, &n_elem_b, &n_elem_a);
+				}	
+				else
+				{
+					while (pos-- > 0)
+					{
+						write(1, "rra\n", 3);
+						r_rotate(lst_a, n_elem_a);
+					}
+					write(1, "pa\n", 3);
+					push(lst_b, &lst_a, &n_elem_b, &n_elem_a);
+				}		
+			}
+		}
+		pos = find_position(lst_a, min);
+		if (pos <= n_elem_a / 2)
+		{
+			while (pos-- > 0)
+			{
+				write(1, "ra\n", 3);
+				rotate(lst_a, n_elem_a);}
+			}	
+		else
+		{
+			while (pos-- > 0)
+			{
+				write(1, "rra\n", 3);
+				r_rotate(lst_a, n_elem_a);
+			}
+		}
+	}
+}
+
+int find_position(t_list **lst, int n)
+{
+	t_list	*aux;
+	int	pos;
+	
+	pos = 0;
+	aux = *lst;
+	while(aux)
+	{
+		if ((*(int *)lst[0]->content) == n)
+			return(pos);
+		pos++;
+	}
+	return (pos);
+}
+
+int find_position_insert(t_list **lst, int n)
+{
+	t_list	*aux;
+	int	pos;
+	
+	pos = 0;
+	aux = *lst;
+	if (aux)
+	{
+		while(aux->next)
+		{
+			if (((*(int *)lst[0]->content) < n) && (*(int *)lst[0]->next->content) > n))
+				return(pos + 1);
+			pos++;
+		}
+	}
+	return (0);
+}
+
+int find_min_max(t_list **lst, int min)
+{
+	int	n;
+	t_list	*aux;
+	
+	aux = *lst;
+	n = 0;
+	if (min)
+	{
+		while (aux)
+		{
+			if ((*(int *)aux->content) < n)
+				n = *(int *)aux->content;
+			aux = aux->next;
+		}
+	}
+	else
+	{
+		while (aux)
+		{
+			if ((*(int *)aux->content) > n)
+				n = *(int *)aux->content;
+			aux = aux->next;
+		}
+	}
+	return (n);
+}
+
 int	main(int argc, char **argv)
 {
 	t_list	*lst;
+	int n_elem;
 
 	if ((argc < 1) || !is_valid_arg(argc, argv))
 		print_error();
 	else
 	{
-		get_args(argc, argv, &lst);
+		n_elem = get_args(argc, argv, &lst);
 		repeated_elements(&lst);
+		if ((n_elem_a > 1) && (n_elem_a <= 3))
+			sort_3n(&lst, n_elem);
+		else if ((n_elem_a > 3) && (n_elem_a <= 5))
+			sort_5n(&lst, n_elem);
+		else if ((n_elem_a > 5) && (n_elem_a <= 100))
+			sort_100n(&lst, n_elem);
+		else if (n_elem_a > 100)
+			sort_500n(&lst, n_elem);
 		print_list(&lst);
 		free_list(&lst);
 	}
